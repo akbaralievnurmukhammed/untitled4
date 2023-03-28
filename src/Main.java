010793f7f6c8b98256082d56f40c144f6959fdc3
akbaralievnurmukhammed <122470802+akbaralievnurmukhammed@users.noreply.github.com>
2023-03-28 20:36:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Scanner;
+
+import static java.langa.StringUTF16.charAt;
+
+public class Main {
+    public static void main(String[] args) {
+        String a = "Nurmukhammed";
+        System.out.println(charAt(a));
+   }
+   }
+
+
